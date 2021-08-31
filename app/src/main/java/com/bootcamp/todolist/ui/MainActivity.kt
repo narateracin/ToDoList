d77ab4817f3813c41629db2597946d63ad50e7f9
Nara <nara.teracin@gmail.com>
2021-08-31 10:55:29 -0300

    Final Project to Santander Bootcamp

---
+++ app/src/main/java/com/bootcamp/todolist/ui/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.bootcamp.todolist.ui
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.bootcamp.todolist.databinding.ActivityMainBinding
+import com.bootcamp.todolist.datasource.TaskDataSource
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter()}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
+        updateList()
+
+        insertListener()
+        //data store
+        //room
+    }
+
+    private fun insertListener() {
+        binding.fab.setOnClickListener {
+            startActivityForResult(Intent(this, AddTaskActivity::class.java), CREATE_NEW_TASK)
+        }
+
+        adapter.listenerEdit = {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            startActivityForResult(intent, CREATE_NEW_TASK)
+        }
+
+        adapter.listenerDelete = {
+            TaskDataSource.deleteTask(it)
+            updateList()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CREATE_NEW_TASK && resultCode == Activity.RESULT_OK) {
+            binding.rvTasks.adapter = adapter
+            adapter.submitList(TaskDataSource.getList())
+        }
+    }
+
+    private fun updateList() {
+        val list = TaskDataSource.getList()
+
+        binding.includeEmpty.emptyState.visibility = if(list.isEmpty()) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+        adapter.submitList(list)
+    }
+
+    companion object {
+        private const val CREATE_NEW_TASK = 1000
+    }
+}
